f79f5e5df32807fa5b282ab68e70f3fa06fef93e
Ravi.Teja <you@example.com>
2022-03-31 19:56:15 +0530

    update

--- Create_LB_Basic.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param(
-    [Parameter(Mandatory=$true)][string]$resourceGroupName,
-    [Parameter(Mandatory=$true)][string]$lbName
-)
-
-
-az network lb create -g $resourceGroupName -n $lbName --sku Basic
